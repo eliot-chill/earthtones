38ec194fd848dcb99a038874f37e1e0fb16cca85
LazyCrazyTom <tclare50@gmail.com>
2020-01-25 16:38:44 +0000

    early weather API

--- webportal.py
+++ webportal.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 from test import testFunc
+
 app = Flask(__name__)
+
+import update_wind
 
 @app.route("/")
 def hello():
@@ -9,6 +12,10 @@ def hello():
 @app.route("/test")
 def testWrapper():
    return  testFunc()
+
+@app.route("/update_wind")
+def update_wind():
+    return update_wind.call()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', threaded=True, debug=True)

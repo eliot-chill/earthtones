10ddbd58f1a307f6311eee465ed5db54cae2dd61
LazyCrazyTom <tclare50@gmail.com>
2020-01-26 09:19:31 +0000

    Heatmaps!

--- dataProcessor.py
+++ dataProcessor.py
@@ -1,27 +1,32 @@
 import db
 
 def getArduinosForMarkers():
-    earthtonesDB = db.dbConn()
-    activeArduinosCollection= earthtonesDB.activeArduinos
-    arduinoList = []
-    for arduino in activeArduinosCollection.find({},{ "_id": 0}):
-        arduinoList.append(arduino)
-    return arduinoList
+	earthtonesDB = db.dbConn()
+	activeArduinosCollection = earthtonesDB.activeArduinos
+	sensorDataCollection = earthtonesDB.sensorData
+	arduinoList = []
+	# ,{ "_id": 0}
+	for arduino in activeArduinosCollection.find({}, {'_id': 0}):
+		arduinoSensors = sensorDataCollection.find_one({'arduinoID': arduino['arduinoID']})
+		arduino["temp"] = arduinoSensors["temperature"]
+		arduino["hum"] = arduinoSensors["humidity"]
+		arduinoList.append(arduino)
+	return arduinoList
 
 def parseUploadData(data):
-    earthtonesDB = db.dbConn()
-    sensorDataCollection = earthtonesDB.sensorData
-    post = {
+	earthtonesDB = db.dbConn()
+	sensorDataCollection = earthtonesDB.sensorData
+	post = {
 		"arduinoID":data['id'],
-	    "temperature":data['temperature'],
-	    "humidity":data['humidity'],
-	    "timestamp":data['timestamp']
+		"temperature":data['temperature'],
+		"humidity":data['humidity'],
+		"timestamp":data['timestamp']
 	}
 
-    sensorDataCollection.update_one({'arduinoID':data['id']}, {"$set": post}, upsert=True)
+	sensorDataCollection.update_one({'arduinoID':data['id']}, {"$set": post}, upsert=True)
 
-    return "Done"
+	return "Done"
 
 
 if __name__ == "__main__":
-    print(getArduinosForMarkers())
+	print(getArduinosForMarkers())

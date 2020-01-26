bb8e1205a30cc82192e6698175e23b7a4eca3b0b
Eliot Cholerton-Hill <eliotchill99@gmail.com>
2020-01-26 02:10:27 +0000

    Added stylish map system

--- webportal.py
+++ webportal.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template, request, jsonify
-from dataProcessor import parseUploadData
+from dataProcessor import parseUploadData, getArduinosForMarkers
 import update_wind
 
 app = Flask(__name__)
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return render_template("map.html",arduinoList=getArduinosForMarkers())
 
 @app.route("/update_wind")
 def update_wind():
